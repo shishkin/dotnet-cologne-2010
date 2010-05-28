cb831b200100f659a2def9b39bdfc712599464d4
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-05-28 16:45:57 +0200

    asp.net fileless hosting

---
+++ AspNetWebService/EchoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+
+namespace AspNetWebService
+{
+    [ServiceContract]
+    public class EchoService
+    {
+        [OperationContract]
+        public string Echo(string text)
+        {
+            return text;
+        }
+    }
+}
