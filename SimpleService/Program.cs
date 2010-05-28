0085cda41f5a5240afe224b7d69ff8231a09dde1
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-05-28 16:34:26 +0200

    first service

---
+++ SimpleService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace SimpleService
+{
+    [ServiceContract]
+    public class EchoService
+    {
+        [OperationContract]
+        public string Echo(string text)
+        {
+            return text;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(
+                typeof(EchoService),
+                new Uri("http://localhost:8081/Echo")))
+            {
+                host.Open();
+
+                foreach (var endpoint in host.Description.Endpoints)
+                {
+                    Console.WriteLine("{0} ({1})",
+                        endpoint.Address,
+                        endpoint.Binding.Name);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
